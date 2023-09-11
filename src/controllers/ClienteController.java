2f3b676d6b65a55a61d91779669743abfc3a5bfc
Alex Christian <alexchristianqr@gmail.com>
2023-09-10 22:50:41 -0500

    [added] metodos de prueba para implementar en controlador cliente

--- src/controllers/ClienteController.java
+++ src/controllers/ClienteController.java
@@ -1,6 +1,20 @@
-
 package controllers;
 
+import models.Cliente;
+
 public class ClienteController {
-    
+
+    public Cliente crearCliente() {
+        Cliente cliente = new Cliente(0, 0, "", "", "", 0, "", 0);
+        return cliente;
+    }
+
+    public Cliente actualizarCliente() {
+        Cliente cliente = new Cliente(0, 0, "", "", "", 0, "", 0);
+        return cliente;
+    }
+
+    public void eliminarCliente(int codigo) {
+        Cliente cliente = new Cliente(0, 0, "", "", "", 0, "", 0);
+    }
 }
