bdca7a811fcde809abd2dfeb3a7a194cff497ad4
Alex Christian <alexchristianqr@gmail.com>
2023-09-26 01:43:42 -0500

    [updated]

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -13,9 +13,9 @@ public class Main {
 
     public static void main(String[] args) {
         ViewMenuPrincipal viewMenuPrincipal = new ViewMenuPrincipal();// Crear objeto del JFrame principal
-        viewMenuPrincipal.setExtendedState(ViewMenuPrincipal.MAXIMIZED_BOTH); 
+        viewMenuPrincipal.setExtendedState(ViewMenuPrincipal.MAXIMIZED_BOTH);
         viewMenuPrincipal.setVisible(true);// Visualizar frame
-        
+
         test();
     }
 
