36bc87a200f48b6fd84cf5ab9369d819aed53115
Alex Christian <alexchristianqr@gmail.com>
2023-11-16 01:17:52 -0500

    [updated] mysql driver

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -1,7 +1,9 @@
 package controllers;
 
+import core.services.Conexion;
 import core.services.IProviderDB;
 import core.services.LocalDBService;
+import java.sql.*;
 import views.ViewMenuPrincipal;
 
 public class Main {
@@ -11,7 +13,10 @@ public class Main {
         viewMenuPrincipal.setExtendedState(ViewMenuPrincipal.MAXIMIZED_BOTH);
         viewMenuPrincipal.setVisible(true);// Visualizar frame
 
-        test();
+        queryConsultar();
+        queryInsertar();
+        queryActualizar();
+        queryEliminar();
     }
 
     public static void test() {
@@ -19,6 +24,105 @@ public class Main {
         proveedor.cargarDB();
         String[] midata = {"alex"};
         proveedor.guardarDB(midata);
+    }
+
+    public static void queryConsultar() {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+            conn = Conexion.obtenerConexion();
+
+            stmt = conn.prepareStatement("SELECT * FROM country where code = ?");
+            stmt.setString(1, "ABW");
+            
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                String code = rs.getString("code");
+                String name = rs.getString("name");
+                System.out.println("Code: " + code + " Name: " + name);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            Conexion.cerrarConsulta(stmt);
+            Conexion.cerrarConexion(conn);
+        }
+    }
+
+    public static int queryInsertar() {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+            conn = Conexion.obtenerConexion();
+
+            stmt = conn.prepareStatement("insert into city (Name, CountryCode, District, Population) values (?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, "TEST");
+            stmt.setString(2, "PER");
+            stmt.setString(3, "TEST");
+            stmt.setInt(4, 1045);
+
+            ResultSet rs = stmt.getGeneratedKeys();
+
+            if (rs.next()) {
+                int code = rs.getInt(1);
+                System.out.println("Code: " + code);
+            }
+
+            return stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            // e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            Conexion.cerrarConsulta(stmt);
+            Conexion.cerrarConexion(conn);
+        }
+    }
+
+    public static int queryActualizar() {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+            conn = Conexion.obtenerConexion();
+
+            stmt = conn.prepareStatement("update city set name = ? where id = ?");
+            stmt.setString(1, "Alex");
+            stmt.setInt(2, 4082);
+
+            return stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            // e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            Conexion.cerrarConsulta(stmt);
+            Conexion.cerrarConexion(conn);
+        }
+    }
+
+    public static int queryEliminar() {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+            conn = Conexion.obtenerConexion();
+
+            stmt = conn.prepareStatement("delete from city where id = ?");
+            stmt.setInt(1, 4082);
+
+            return stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            // e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            Conexion.cerrarConsulta(stmt);
+            Conexion.cerrarConexion(conn);
+        }
     }
 
 }
---
+++ src/core/services/Conexion.java
@@ -0,0 +1,47 @@
+package core.services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Conexion {
+    private static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://127.0.0.1/world";
+    private static final String USER = "root";
+    private static final String PASSWORD = "3VmMWsbo,$9w";
+
+    public static Connection obtenerConexion() {
+        try {
+            Class.forName(DRIVER_NAME);
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            // e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void cerrarConexion(Connection con) {
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                // e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void cerrarConsulta(Statement stmt) {
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                // e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
+}
