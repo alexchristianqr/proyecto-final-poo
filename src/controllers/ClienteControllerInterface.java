e2612c4d8b79e270ef9378cbbd191ca79302bdbe
Alex Christian <alexchristianqr@gmail.com>
2023-12-01 17:26:18 -0500

    [added] Cliente servicio mysql

--- src/controllers/ClienteControllerInterface.java
+++
@@ -1,9 +0,0 @@
-package controllers;
-
-import javax.swing.table.DefaultTableModel;
-import models.Cliente;
-
-public interface ClienteControllerInterface {
-    DefaultTableModel listarClientes(String buscar);
-    Cliente crearCliente(Cliente cliente);
-}
